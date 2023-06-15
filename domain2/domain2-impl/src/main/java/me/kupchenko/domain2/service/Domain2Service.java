b23d00fceca3ec3efe21c50ca8c13022c0a8e7d3
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2023-06-15 23:10:15 +0200

    Resolve known gradle issue: https://github.com/gradle/gradle/issues/847

--- domain2/domain2-impl/src/main/java/me/kupchenko/domain2/service/Domain2Service.java
+++ domain2/domain2-impl/src/main/java/me/kupchenko/domain2/service/Domain2Service.java
@@ -2,6 +2,7 @@ package me.kupchenko.domain2.service;
 
 import lombok.AllArgsConstructor;
 import me.kupchenko.domain1.api.Domain1Client;
+import me.kupchenko.domain1.api.Domain1Dto1;
 import me.kupchenko.domain2.api.Domain2Dto1;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ public class Domain2Service {
   private final Domain1Client domain1Client;
 
   public Domain2Dto1 getDomain2Dto1() {
-    return new Domain2Dto1("1", "2");
+    Domain1Dto1 domain1Dto1 = domain1Client.domain1TestEndpoint();
+    return new Domain2Dto1("1", domain1Dto1.getField());
   }
 }
